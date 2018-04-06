8113a8100c4cf2b3d633c01aea557b4b2af28e82
Dennis Rasmussen <denn5682@gmail.com>
2018-04-06 12:03:42 +0200

    Seperated Web into own solution

---
+++ RPGWeb/RPGWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RPGWeb.Startup))]
+namespace RPGWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
