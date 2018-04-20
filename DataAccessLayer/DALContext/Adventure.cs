1c2bd96592e641bc5bc7a67cc5b3e395960ffddc
Dennis Rasmussen <denn5682@gmail.com>
2018-04-20 12:21:13 +0200

    added Dal part, which is the DatabaseClasses to be used with Entity Framework

---
+++ DataAccessLayer/DALContext/Adventure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALContext
+{
+    public class Adventure
+    {
+        public List<User> players { get; set; }
+        public User GM { get; set; }
+
+    }
+}
---
+++ DataAccessLayer/DALContext/Rumor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALContext
+{
+    public class Rumor
+    {
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Rumor(string description)
+        {
+            Date = DateTime.Now;
+            Description = description;
+        }
+
+    }
+}
